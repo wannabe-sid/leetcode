c5187649fafcc8caccde4e51686babc2e57fd3ad
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-14 10:33:47 +0530

    Time: 44 ms (77.4%), Space: 157.1 MB (82.11%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -33,35 +33,67 @@
 
 // Optimal Solution
 // min{O(k*log(m*n)), O(k*log(k))} time and O(m*n) space
+// class Solution {
+// public:
+//     typedef pair<int, pair<int, int>> P;
+//     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+//         priority_queue<P, vector<P>, greater<P>> pq;
+//         int m = nums1.size();
+//         int n = nums2.size();
+//         int sum = nums1[0] + nums2[0];
+//         set<pair<int, int>> visited;
+//         pq.push({sum, {0, 0}});
+//         visited.insert({0, 0});
+//         vector<vector<int>> result;
+//         while(k-- && !pq.empty()){
+//             auto temp = pq.top();
+//             pq.pop();
+//             int i = temp.second.first;
+//             int j = temp.second.second;
+//             result.push_back({nums1[i], nums2[j]});
+//             // Push (i, j+1) if possible
+//             if(j+1 < n && visited.find({i, j+1}) == visited.end()){
+//                 pq.push({nums1[i] + nums2[j+1], {i, j+1}});
+//                 visited.insert({i, j+1});
+//             }
+//             // Push (i+1, j) if possible
+//             if(i+1 < m && visited.find({i+1, j}) == visited.end()){
+//                 pq.push({nums1[i+1] + nums2[j], {i+1, j}});
+//                 visited.insert({i+1, j});
+//             }
+//         }
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
-    typedef pair<int, pair<int, int>> P;
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        priority_queue<P, vector<P>, greater<P>> pq;
-        int m = nums1.size();
-        int n = nums2.size();
-        int sum = nums1[0] + nums2[0];
-        set<pair<int, int>> visited;
-        pq.push({sum, {0, 0}});
-        visited.insert({0, 0});
         vector<vector<int>> result;
-        while(k-- && !pq.empty()){
-            auto temp = pq.top();
-            pq.pop();
-            int i = temp.second.first;
-            int j = temp.second.second;
+        if (nums1.empty() || nums2.empty() || k <= 0) return result;
+
+        // Min-heap storing tuples: {sum, index1, index2}
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> minHeap;
+
+        // Push the initial pairs (nums1[i], nums2[0]) for i up to min(k, nums1.size())
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        for (int i = 0; i < min((int)n1, k); ++i) {
+            minHeap.push({nums1[i] + nums2[0], i, 0});
+        }
+
+        // Pop the smallest pair and push the next pair (nums1[i], nums2[j + 1])
+        while (k-- > 0 && !minHeap.empty()) {
+            auto [currentSum, i, j] = minHeap.top();
+            minHeap.pop();
+
             result.push_back({nums1[i], nums2[j]});
-            // Push (i, j+1) if possible
-            if(j+1 < n && visited.find({i, j+1}) == visited.end()){
-                pq.push({nums1[i] + nums2[j+1], {i, j+1}});
-                visited.insert({i, j+1});
-            }
-            // Push (i+1, j) if possible
-            if(i+1 < m && visited.find({i+1, j}) == visited.end()){
-                pq.push({nums1[i+1] + nums2[j], {i+1, j}});
-                visited.insert({i+1, j});
+
+            if (j + 1 < n2) {
+                minHeap.push({nums1[i] + nums2[j + 1], i, j + 1});
             }
         }
+
         return result;
     }
 };
