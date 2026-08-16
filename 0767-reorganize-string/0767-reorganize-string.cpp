0d439e733af59a15c77a65bb0137a765dda76c36
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-16 10:23:19 +0530

    Time: 0 ms (100%), Space: 8.3 MB (79.89%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,47 +1,76 @@
 // Approach 1 : Using Heap
-// O() time and O() space
+// O(nlogn) time and O() space
 // If frequency of any character in the string is greater than (n+1)/2, then that string cannot be reorganized
+// class Solution {
+// public:
+//     string reorganizeString(string s) {
+//         int n = s.length();
+//         vector<int> freq(26, 0);
+//         for(char& ch : s){
+//             freq[ch - 'a']++;
+//             if(freq[ch - 'a'] > (n + 1) / 2){
+//                 return "";
+//             }
+//         }
+//         priority_queue<pair<int, char>> maxHeap; // pair<frequency, character>
+//         for(int i=0; i<26; i++){
+//             char ch = i + 'a';
+//             if(freq[i] > 0){
+//                 maxHeap.push({freq[i], ch});
+//             }
+//         }
+//         string result = "";
+//         while(maxHeap.size() >= 2){
+//             auto p1 = maxHeap.top();
+//             maxHeap.pop();
+//             auto p2 = maxHeap.top();
+//             maxHeap.pop();
+//             result.push_back(p1.second);
+//             p1.first--;
+//             result.push_back(p2.second);
+//             p2.first--;
+//             if(p1.first > 0) maxHeap.push(p1);
+//             if(p2.first > 0) maxHeap.push(p2);
+//         }
+//         if(!maxHeap.empty()) result.push_back(maxHeap.top().second);
+//         return result;
+//     }
+// };
+
+// Approach 2
+// O(n) time and O(1) space
 class Solution {
 public:
     string reorganizeString(string s) {
         int n = s.length();
         vector<int> freq(26, 0);
+        int maxFreq = 0;
+        char maxFreqChar;
         for(char& ch : s){
             freq[ch - 'a']++;
+            if(freq[ch - 'a'] > maxFreq){
+                maxFreq = freq[ch - 'a'];
+                maxFreqChar = ch;
+            }
             if(freq[ch - 'a'] > (n + 1) / 2){
                 return "";
             }
         }
-        priority_queue<pair<int, char>> maxHeap; // pair<frequency, character>
-        for(int i=0; i<26; i++){
-            char ch = i + 'a';
-            if(freq[i] > 0){
-                maxHeap.push({freq[i], ch});
+        string result = s;
+        int i = 0;
+        while(freq[maxFreqChar - 'a'] > 0){
+            result[i] = maxFreqChar;
+            i += 2;
+            freq[maxFreqChar - 'a']--;
+        }
+        for(char ch='a'; ch<='z'; ch++){
+            while(freq[ch - 'a'] > 0){
+                if(i >= n) i = 1;
+                result[i] = ch;
+                i += 2;
+                freq[ch - 'a']--;
             }
         }
-        string result = "";
-        while(maxHeap.size() >= 2){
-            auto p1 = maxHeap.top();
-            maxHeap.pop();
-            auto p2 = maxHeap.top();
-            maxHeap.pop();
-            result.push_back(p1.second);
-            p1.first--;
-            result.push_back(p2.second);
-            p2.first--;
-            if(p1.first > 0) maxHeap.push(p1);
-            if(p2.first > 0) maxHeap.push(p2);
-        }
-        if(!maxHeap.empty()) result.push_back(maxHeap.top().second);
         return result;
     }
 };
-
-// Approach 2
-// O() time and O() space
-// class Solution {
-// public:
-//     string reorganizeString(string s) {
-        
-//     }
-// };
