bdf26261214817d64bd1c0abb943d7a0b76c2139
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-16 22:19:01 +0530

    Time: 946 ms (12.9%), Space: 178.8 MB (13.39%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,31 +1,47 @@
 // Approach 1 : Using MaxHeap
 // O(n^2 * logk) time and O(k) space
-class Solution {
-public:
-    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        int n = arr.size();
-        priority_queue<vector<double>> maxHeap; // maxHeap<fraction, arr[i], arr[j]>
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                double div = (double) arr[i] / arr[j];
-                maxHeap.push(vector<double>{div, (double)arr[i], (double)arr[j]});
-                if(maxHeap.size() > k) maxHeap.pop();
-            }
-        }
-        auto vec = maxHeap.top();
-        vector<int> result(2);
-        result[0] = vec[1];
-        result[1] = vec[2];
-        return result;
-    }
-};
-
-// Approach 2 : Using MinHeap
-// O() time and O() space
 // class Solution {
 // public:
 //     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
 //         int n = arr.size();
 //         priority_queue<vector<double>> maxHeap; // maxHeap<fraction, arr[i], arr[j]>
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 double div = (double) arr[i] / arr[j];
+//                 maxHeap.push(vector<double>{div, (double)arr[i], (double)arr[j]});
+//                 if(maxHeap.size() > k) maxHeap.pop();
+//             }
+//         }
+//         auto vec = maxHeap.top();
+//         vector<int> result(2);
+//         result[0] = vec[1];
+//         result[1] = vec[2];
+//         return result;
 //     }
 // };
+
+// Approach 2 : Using MinHeap
+// O(nlogn + klogn) time and O(k) space
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        int n = arr.size();
+        priority_queue<vector<double>, vector<vector<double>>, greater<vector<double>>> minHeap; // minHeap<fraction, i, j>
+        for(int i=0; i<n; i++){
+            minHeap.push({(double) arr[i] / arr[n - 1], (double) i, (double) (n - 1)});
+        }
+        int smallest = 1;
+        while(smallest < k){
+            vector<double> vec = minHeap.top();
+            minHeap.pop();
+            int i = vec[1];
+            int j = vec[2] - 1;
+            minHeap.push({(double) arr[i] / arr[j], (double) i, (double) j});
+            smallest++;
+        }
+        vector<double> vec = minHeap.top();
+        int i = vec[1];
+        int j = vec[2];
+        return {arr[i], arr[j]};
+    }
+};
