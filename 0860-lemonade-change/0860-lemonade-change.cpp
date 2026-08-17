132a3554d0d9004df855b8f7cfb6bf0c63e3b084
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-17 11:30:20 +0530

    Time: 0 ms (100%), Space: 87.1 MB (96.75%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -6,18 +6,18 @@ public:
         int fives = 0;
         int tens  = 0;
         for(int i=0; i<n; i++){
-            if(bills[i] == 5) fives += 1;
+            if(bills[i] == 5) fives++;
             else if(bills[i] == 10){
-                if(fives){
-                    fives -= 1;
-                    tens += 1;
+                if(fives >= 1){
+                    fives--;
+                    tens++;
                 }
                 else return false;
             }
             else{
-                if(tens && fives){
-                    tens -= 1;
-                    fives -= 1;
+                if(tens >=1 && fives >= 1){
+                    tens--;
+                    fives--;
                 }
                 else if(fives >= 3) fives -= 3;
                 else return false;
