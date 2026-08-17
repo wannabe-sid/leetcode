5805198e3dc44aced3f5851321a91fdfde202c75
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-17 09:35:09 +0530

    Time: 8 ms (81.12%), Space: 29.5 MB (95.32%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.cpp
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.cpp
@@ -66,33 +66,62 @@
 // };
 
 // Optimal Solution : O(nlogn + klogk + nlogk) time and O(n + k) space
+// class Solution {
+// public:
+//     double mincostToHireWorkers(vector<int>& quality, vector<int>& min_wage, int k) {
+//         int n = quality.size();
+//         vector<pair<double, int>> worker_ratio(n); // pair<workerRatio, workerQuality>
+//         for(int worker = 0; worker < n; worker++) {
+//             worker_ratio[worker] = make_pair((double)min_wage[worker]/quality[worker], quality[worker]);
+//         }
+//         sort(begin(worker_ratio), end(worker_ratio));
+//         priority_queue<int, vector<int>> pq;      
+//         double sum_quality = 0;
+//         for(int i = 0; i < k; i++) { 
+//             pq.push(worker_ratio[i].second); //push all qualities in max-heap
+//             sum_quality += worker_ratio[i].second; //Find sum of qualities
+//         }
+//         double managerRatio = worker_ratio[k-1].first; 
+//         double result       = managerRatio * sum_quality;
+//         for(int manager = k; manager < n; manager++) {         
+//             managerRatio = worker_ratio[manager].first;
+//             pq.push(worker_ratio[manager].second); //push all qualities in max-heap
+//             sum_quality += worker_ratio[manager].second; //Find sum of qualities
+//             if(pq.size() > k) {
+//                 sum_quality -= pq.top();
+//                 pq.pop();
+//             }
+//             result = min(result, managerRatio*sum_quality);
+//         }
+//         return result;
+//     }
+// };
+
+// Most Optimal Solution : O(nlon + nlogk) time and O(n + k) space
 class Solution {
 public:
-    double mincostToHireWorkers(vector<int>& quality, vector<int>& min_wage, int k) {
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
         int n = quality.size();
-        vector<pair<double, int>> worker_ratio(n); // pair<workerRatio, workerQuality>
-        for(int worker = 0; worker < n; worker++) {
-            worker_ratio[worker] = make_pair((double)min_wage[worker]/quality[worker], quality[worker]);
+        vector<pair<double, int>> workers(n);
+        for(int i = 0; i < n; ++i) {
+            double ratio = (double) wage[i] / quality[i];
+            workers[i] = {ratio, quality[i]};
         }
-        sort(begin(worker_ratio), end(worker_ratio));
-        priority_queue<int, vector<int>> pq;      
-        double sum_quality = 0;
-        for(int i = 0; i < k; i++) { 
-            pq.push(worker_ratio[i].second); //push all qualities in max-heap
-            sum_quality += worker_ratio[i].second; //Find sum of qualities
-        }
-        double managerRatio = worker_ratio[k-1].first; 
-        double result       = managerRatio * sum_quality;
-        for(int manager = k; manager < n; manager++) {         
-            managerRatio = worker_ratio[manager].first;
-            pq.push(worker_ratio[manager].second); //push all qualities in max-heap
-            sum_quality += worker_ratio[manager].second; //Find sum of qualities
-            if(pq.size() > k) {
-                sum_quality -= pq.top();
-                pq.pop();
+        sort(workers.begin(), workers.end());
+        priority_queue<int> maxHeap;
+        int totalQuality = 0;
+        double minCost = DBL_MAX;
+        for(int i = 0; i < n; ++i) {
+            totalQuality += workers[i].second;
+            if(i >= k - 1) {
+                minCost = min(minCost, totalQuality * workers[i].first);
             }
-            result = min(result, managerRatio*sum_quality);
+            maxHeap.push(workers[i].second);
+            if(maxHeap.size() > k - 1) {
+                totalQuality -= maxHeap.top();
+                maxHeap.pop();
+            }
         }
-        return result;
+        return minCost;
     }
 };
