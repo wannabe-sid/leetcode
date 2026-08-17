05354f709895aef1bfc802e2195980a74a0052d6
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-17 12:24:50 +0530

    Time: 0 ms (100%), Space: 20.7 MB (54.53%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -17,31 +17,44 @@
 
 // Approach 2 : Using Recursion + DP
 // O(n^2) time and O(n) space
-class Solution {
-public:
-    int solve(vector<int>& nums, int idx, vector<int>& dp) {
-        if (idx >= nums.size() - 1) return 0;   
-        if (dp[idx] != -1) return dp[idx];       
-        int mini = 1e9;        
-        for (int i = 1; i <= nums[idx]; i++) {
-            if (idx + i < nums.size()) {
-                mini = min(mini, 1 + solve(nums, idx + i, dp));
-            }
-        }        
-        return dp[idx] = mini;
-    }
-    int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return solve(nums, 0, dp);
-    }
-};
-
-// Approach 2 : Using Greedy Method
-// O() time and O() space
 // class Solution {
 // public:
+//     int solve(vector<int>& nums, int idx, vector<int>& dp) {
+//         if (idx >= nums.size() - 1) return 0;   
+//         if (dp[idx] != -1) return dp[idx];       
+//         int mini = 1e9;        
+//         for (int i = 1; i <= nums[idx]; i++) {
+//             if (idx + i < nums.size()) {
+//                 mini = min(mini, 1 + solve(nums, idx + i, dp));
+//             }
+//         }        
+//         return dp[idx] = mini;
+//     }
 //     int jump(vector<int>& nums) {
-        
+//         int n = nums.size();
+//         vector<int> dp(n, -1);
+//         return solve(nums, 0, dp);
 //     }
 // };
+
+// Approach 3 : Using Greedy Method
+// O(n) time and O(1) space
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = 0;
+        int jumps = 0;
+        while(right < n - 1){
+            int farthest = 0;
+            for(int idx=left; idx<=right; idx++){
+                farthest = max(farthest, idx + nums[idx]);
+            }
+            left = right + 1;
+            right = farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+};
