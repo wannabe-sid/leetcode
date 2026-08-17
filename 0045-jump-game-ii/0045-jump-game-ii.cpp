50efb14c95b721f6ca1d893dbb2d90bad6bea25e
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-17 12:09:26 +0530

    Time: 219 ms (5.89%), Space: 22.7 MB (7.69%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,47 @@
+// Approach 1 : Using Recursion
+// O(n^n) time and O(n) space
+// class Solution {
+// public:
+//     int solve(vector<int>& nums, int idx, int jumps){
+//         if(idx >= nums.size() - 1) return jumps;
+//         int mini = INT_MAX;
+//         for(int i=1; i<=nums[idx]; i++){
+//             mini = min(mini, solve(nums, idx + i, jumps + 1));
+//         }
+//         return mini;
+//     }
+//     int jump(vector<int>& nums) {
+//         return solve(nums, 0, 0);
+//     }
+// };
+
+// Approach 2 : Using Recursion + DP
+// O(n^2) time and O(n) space
+class Solution {
+public:
+    int solve(vector<int>& nums, int idx, vector<int>& dp) {
+        if (idx >= nums.size() - 1) return 0;   
+        if (dp[idx] != -1) return dp[idx];       
+        int mini = 1e9;        
+        for (int i = 1; i <= nums[idx]; i++) {
+            if (idx + i < nums.size()) {
+                mini = min(mini, 1 + solve(nums, idx + i, dp));
+            }
+        }        
+        return dp[idx] = mini;
+    }
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return solve(nums, 0, dp);
+    }
+};
+
+// Approach 2 : Using Greedy Method
+// O() time and O() space
+// class Solution {
+// public:
+//     int jump(vector<int>& nums) {
+        
+//     }
+// };
