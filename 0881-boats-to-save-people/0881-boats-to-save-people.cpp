7172be068b11afcee4ff6eedb157496bf70398e4
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-18 22:50:09 +0530

    Time: 22 ms (24.73%), Space: 45.9 MB (38.98%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,23 @@
+// O(n) time and O(1) space
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        sort(people.begin(), people.end());
+        int i = 0;
+        int j = n - 1;
+        int boats = 0;
+         while(i <= j){
+            if(people[i] + people[j] <= limit){
+                i++;
+                j--;
+                boats++;
+            }
+            else{
+                j--;
+                boats++;
+            }
+        }
+        return boats;
+    }
+};
