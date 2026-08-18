4e7deaa6f4475ba7079996dfcf694be18c36d666
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-18 17:01:55 +0530

    Time: 0 ms (100%), Space: 68.3 MB (5.94%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -25,19 +25,50 @@
 // };
 
 // Better Approach : O(n) time and O(n) space
+// class Solution {
+// public:
+//     int candy(vector<int>& ratings) {
+//         int n = ratings.size();
+//         vector<int> candies(n, 1);
+//         for (int i = 1; i < n; ++i) {
+//             if (ratings[i] > ratings[i - 1])
+//                 candies[i] = candies[i - 1] + 1;
+//         }
+//         for (int i = n - 2; i >= 0; --i) {
+//             if (ratings[i] > ratings[i + 1])
+//                 candies[i] = max(candies[i], candies[i + 1] + 1);
+//         }
+//         return accumulate(candies.begin(), candies.end(), 0);
+//     }
+// };
+
+// Optimal Approach : O(n) time and O(1) space
+// Greedy Approach : Using Slope Concept
 class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> candies(n, 1);
-        for (int i = 1; i < n; ++i) {
-            if (ratings[i] > ratings[i - 1])
-                candies[i] = candies[i - 1] + 1;
+        int candies = n;
+        int i = 1;
+        while (i < n) {
+            if (ratings[i] == ratings[i - 1]) {
+                i++;
+                continue;
+            }
+            int peak = 0;
+            while (i < n && ratings[i] > ratings[i - 1]) {
+                peak++;
+                candies += peak;
+                i++;
+            }
+            int valley = 0;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                valley++;
+                candies += valley;
+                i++;
+            }
+            candies -= min(peak, valley);
         }
-        for (int i = n - 2; i >= 0; --i) {
-            if (ratings[i] > ratings[i + 1])
-                candies[i] = max(candies[i], candies[i + 1] + 1);
-        }
-        return accumulate(candies.begin(), candies.end(), 0);
+        return candies;
     }
 };
