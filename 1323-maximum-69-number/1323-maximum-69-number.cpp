45e8c365b3560851befbc3a4db1e94da6528efb9
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-20 17:27:44 +0530

    Time: 0 ms (100%), Space: 8.1 MB (39.35%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,16 +1,37 @@
 // Approach 1
 // O(d) time and O(d) space
+// class Solution {
+// public:
+//     int maximum69Number (int num) {
+//         string temp = to_string(num);
+//         for(char& ch : temp){
+//             if(ch == '6'){
+//                 ch = '9';
+//                 break;
+//             }
+//         }
+//         num = stoi(temp);
+//         return num;
+//     }
+// };
+
+// Approach 2
+// O(d) time and O(d) space
 class Solution {
 public:
     int maximum69Number (int num) {
-        string temp = to_string(num);
-        for(char& ch : temp){
-            if(ch == '6'){
-                ch = '9';
-                break;
+        int placeValue = 0;
+        int placeValueOfSix = -1;
+        int temp = num;
+        while(temp > 0){
+            int rem = temp % 10;
+            if(rem == 6){
+                placeValueOfSix = placeValue;
             }
+            temp = temp / 10;
+            placeValue++;
         }
-        num = stoi(temp);
-        return num;
+        if(placeValueOfSix == -1) return num;
+        return num + 3 * pow(10, placeValueOfSix);
     }
 };
