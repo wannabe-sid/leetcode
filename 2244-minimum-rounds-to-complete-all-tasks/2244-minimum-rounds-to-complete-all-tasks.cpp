e8a0f6602402831f1fae3344730d47ddcf9d488f
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-20 21:47:27 +0530

    Time: 35 ms (34.2%), Space: 107.4 MB (75.67%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,4 +1,22 @@
 // O(2n) time and O(n) space
+// class Solution {
+// public:
+//     int minimumRounds(vector<int>& tasks) {
+//         unordered_map<int, int> mpp;
+//         for(int& task : tasks){
+//             mpp[task]++;
+//         }
+//         int rounds = 0;
+//         for(auto& it : mpp){
+//             int count = it.second;
+//             if(count == 1) return -1;
+//             rounds += (count + 2)/ 3;
+//         }
+//         return rounds;
+//     }
+// };
+
+// O(2n) time and O(n) space
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
@@ -10,7 +28,8 @@ public:
         for(auto& it : mpp){
             int count = it.second;
             if(count == 1) return -1;
-            rounds += (count + 2) / 3;
+            if(count % 3 == 0) rounds += count / 3;
+            else rounds += count / 3 + 1; 
         }
         return rounds;
     }
