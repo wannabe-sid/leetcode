0ef25827caffe0225bab2f0a2fcfc0a592b868fd
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-20 21:59:56 +0530

    Time: 1 ms (93.63%), Space: 84.5 MB (18.68%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,16 +1,41 @@
+// Approach 1 : Greedy
 // O(n + nlogn) time and O(1) space
+// class Solution {
+// public:
+//     int maxIceCream(vector<int>& costs, int coins) {
+//         sort(costs.begin(), costs.end());
+//         int count = 0;
+//         for(int& cost : costs){
+//             if(cost > coins) return count;
+//             else{
+//                 count++;
+//                 coins -= cost;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+// Approach 2 : Counting Sort
+// O(maxCost) time and O(maxCost) space
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(), costs.end());
-        int count = 0;
-        for(int& cost : costs){
-            if(cost > coins) return count;
-            else{
-                count++;
-                coins -= cost;
-            }
-        }
-        return count;
+        int maxCost = *max_element(begin(costs), end(costs));
+        // count[c] = how many ice creams cost exactly c
+        vector<int> count(maxCost + 1, 0);
+        for (int cost : costs) count[cost]++;
+        int total = 0;
+        for (int cost = 1; cost <= maxCost; cost++) {
+            if (count[cost] == 0) continue;          
+            if (coins < cost) break;            
+            // Buy whatever is possible
+            int bought = min(count[cost], coins / cost);
+            total += bought;
+            coins -= bought * cost;
+        }        
+        return total;
     }
 };
+
+// Approach 3 : Recursion
