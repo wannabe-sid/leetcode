f6c280544dee240f6c7047e4bebe1e39c8db4e07
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-21 11:41:58 +0530

    Time: 245 ms (5.19%), Space: 9.1 MB (96.45%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,39 +1,75 @@
 // Approach-1 (Brute Force)
+// class Solution {
+// public:
+//     bool removeSenator(string &senate, char ch, int idx) {
+//         bool loopAround = false;       
+//         while(true) {          
+//             if(idx == 0) {
+//                 loopAround = true;
+//             }
+//             if(senate[idx] == ch) {
+//                 senate.erase(begin(senate) + idx);
+//                 break;
+//             }
+//             idx = (idx+1)%(senate.length());
+//         }
+//         return loopAround;
+//     }
+//     string predictPartyVictory(string senate) {
+//         int R_Count = count(begin(senate), end(senate), 'R');
+//         int D_Count = senate.length() - R_Count;
+//         int idx = 0;
+//         while(R_Count > 0 && D_Count > 0) {
+//             if(senate[idx] == 'R') {
+//                 bool checkRemoval = removeSenator(senate, 'D', (idx+1)%(senate.length()));
+//                 D_Count--;
+//                 if(checkRemoval) {
+//                     idx--;
+//                 }
+//             } else {
+//                 bool checkRemoval = removeSenator(senate, 'R', (idx+1)%(senate.length()));
+//                 R_Count--;
+//                 if(checkRemoval) {
+//                     idx--;
+//                 }
+//             }
+//             idx = (idx+1)%(senate.length());
+//         }
+//         return R_Count == 0 ? "Dire" : "Radiant";
+//     }
+// };
+
+//Approach-2 (Improved Brute Force)
 class Solution {
 public:
-    bool removeSenator(string &senate, char ch, int idx) {
-        bool loopAround = false;       
-        while(true) {          
-            if(idx == 0) {
-                loopAround = true;
-            }
-            if(senate[idx] == ch) {
-                senate.erase(begin(senate) + idx);
+    int n;
+    void removeSenator(string &senate, vector<bool>& removed, char ch, int idx) {
+        while(true) {     
+            if(senate[idx] == ch && removed[idx] == false) {
+                removed[idx] = true;
                 break;
             }
-            idx = (idx+1)%(senate.length());
+            idx = (idx+1)%n;
         }
-        return loopAround;
     }
     string predictPartyVictory(string senate) {
+        n = senate.length();
         int R_Count = count(begin(senate), end(senate), 'R');
-        int D_Count = senate.length() - R_Count;
+        int D_Count = n - R_Count;
         int idx = 0;
+        //Initially no one removed
+        vector<bool> removed(n, false);
         while(R_Count > 0 && D_Count > 0) {
-            if(senate[idx] == 'R') {
-                bool checkRemoval = removeSenator(senate, 'D', (idx+1)%(senate.length()));
-                D_Count--;
-                if(checkRemoval) {
-                    idx--;
-                }
-            } else {
-                bool checkRemoval = removeSenator(senate, 'R', (idx+1)%(senate.length()));
-                R_Count--;
-                if(checkRemoval) {
-                    idx--;
+            if(!removed[idx]) {
+                if(senate[idx] == 'R') {
+                    removeSenator(senate, removed, 'D', (idx+1)%n);
+                    D_Count--;
+                } else {
+                    removeSenator(senate, removed, 'R', (idx+1)%n);
+                    R_Count--;
                 }
             }
-            idx = (idx+1)%(senate.length());
+            idx = (idx+1)%n;
         }
         return R_Count == 0 ? "Dire" : "Radiant";
     }
