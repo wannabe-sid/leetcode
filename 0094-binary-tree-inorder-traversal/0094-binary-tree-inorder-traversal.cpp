c1dce835d2890f5174364a57e1cfd39a5e1bc5ca
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-24 10:56:40 +0530

    Time: 0 ms (100%), Space: 10.9 MB (66.83%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,16 +12,42 @@
 
 // Recursive
 // O(n^2) time and O(n) space
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         if(root == nullptr) return {};
+//         vector<int> left = inorderTraversal(root -> left);
+//         result.insert(result.end(), left.begin(), left.end());
+//         result.push_back(root -> val);
+//         vector<int> right = inorderTraversal(root -> right);
+//         result.insert(result.end(), right.begin(), right.end());
+//         return result;
+//     }
+// };
+
+// Iterative
+// O(n) time and O(n) space
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> result;
-        if(root == nullptr) return {};
-        vector<int> left = inorderTraversal(root -> left);
-        result.insert(result.end(), left.begin(), left.end());
-        result.push_back(root -> val);
-        vector<int> right = inorderTraversal(root -> right);
-        result.insert(result.end(), right.begin(), right.end());
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        while (curr != nullptr || !st.empty()) {
+            // Reach the leftmost node of the current subtree
+            while (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            // Current is null here, pop from stack
+            curr = st.top();
+            st.pop();
+            // Visit the node
+            result.push_back(curr->val);
+            // Move to the right subtree
+            curr = curr->right;
+        }
         return result;
     }
 };
