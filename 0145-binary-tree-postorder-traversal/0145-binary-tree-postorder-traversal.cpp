2e10086096dd2a34d2939cce5deaa812096da769
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-08-24 11:01:41 +0530

    Time: 0 ms (100%), Space: 10.9 MB (93.5%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -12,42 +12,46 @@
 
 // Recursive
 // O(n^2) time and O(n) space
+// class Solution {
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         if(root == nullptr) return {};
+//         vector<int> left = postorderTraversal(root -> left);
+//         result.insert(result.end(), left.begin(), left.end());
+//         vector<int> right = postorderTraversal(root -> right);
+//         result.insert(result.end(), right.begin(), right.end());
+//         result.push_back(root -> val);
+//         return result;
+//     }
+// };
+
+// Iterative
+// O(n) time and O(n) space
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> result;
-        if(root == nullptr) return {};
-        vector<int> left = postorderTraversal(root -> left);
-        result.insert(result.end(), left.begin(), left.end());
-        vector<int> right = postorderTraversal(root -> right);
-        result.insert(result.end(), right.begin(), right.end());
-        result.push_back(root -> val);
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        TreeNode* lastVisited = nullptr;
+        while (curr != nullptr || !st.empty()) {
+            // Reach the leftmost node of the current subtree
+            while (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            TreeNode* peekNode = st.top();
+            // If right child exists and traversing from the left child, move right
+            if (peekNode->right != nullptr && lastVisited != peekNode->right) {
+                curr = peekNode->right;
+            } else {
+                // Visit the node
+                result.push_back(peekNode->val);
+                lastVisited = peekNode;
+                st.pop();
+            }
+        }
         return result;
     }
 };
-
-// Iterative
-// O(n) time and O(n) space
-// class Solution {
-// public:
-//     vector<int> inorderTraversal(TreeNode* root) {
-//         vector<int> result;
-//         stack<TreeNode*> st;
-//         TreeNode* curr = root;
-//         while (curr != nullptr || !st.empty()) {
-//             // Reach the leftmost node of the current subtree
-//             while (curr != nullptr) {
-//                 st.push(curr);
-//                 curr = curr->left;
-//             }
-//             // Current is null here, pop from stack
-//             curr = st.top();
-//             st.pop();
-//             // Visit the node
-//             result.push_back(curr->val);
-//             // Move to the right subtree
-//             curr = curr->right;
-//         }
-//         return result;
-//     }
-// };
