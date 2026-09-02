165f2d5fd7ec9e3b158486ce5e095d3a68d4a54a
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-09-02 12:49:45 +0530

    Time: 2 ms (98.98%), Space: 128.1 MB (97.09%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -11,50 +11,77 @@
  */
 
 // O(n) time and O(n) space
+// class Solution {
+// public:
+//     unordered_map<TreeNode*, TreeNode*> parent;
+//     void buildParent(TreeNode* root, TreeNode* par) {
+//         if(!root) return;
+//         parent[root] = par;
+//         buildParent(root->left, root);
+//         buildParent(root->right, root);
+//     }
+//     TreeNode* findNode(TreeNode* root, int start) {
+//         if(!root) return nullptr;
+//         if(root->val == start) return root;
+//         TreeNode* left = findNode(root->left, start);
+//         if (left) return left;
+//         return findNode(root->right, start);
+//     }
+//     int amountOfTime(TreeNode* root, int start) {
+//         buildParent(root, nullptr);
+//         TreeNode* startNode = findNode(root, start);
+//         queue<TreeNode*> q;
+//         unordered_set<TreeNode*> vis;
+//         q.push(startNode);
+//         vis.insert(startNode);
+//         int time = -1;
+//         while (!q.empty()) {
+//             int sz = q.size();
+//             time++;
+//             while (sz--) {
+//                 TreeNode* curr = q.front();
+//                 q.pop();
+//                 if (curr->left && !vis.count(curr->left)) {
+//                     vis.insert(curr->left);
+//                     q.push(curr->left);
+//                 }
+//                 if (curr->right && !vis.count(curr->right)) {
+//                     vis.insert(curr->right);
+//                     q.push(curr->right);
+//                 }
+//                 if (parent[curr] && !vis.count(parent[curr])) {
+//                     vis.insert(parent[curr]);
+//                     q.push(parent[curr]);
+//                 }
+//             }
+//         }
+//         return time;
+//     }
+// };
+
 class Solution {
 public:
-    unordered_map<TreeNode*, TreeNode*> parent;
-    void buildParent(TreeNode* root, TreeNode* par) {
-        if(!root) return;
-        parent[root] = par;
-        buildParent(root->left, root);
-        buildParent(root->right, root);
-    }
-    TreeNode* findNode(TreeNode* root, int start) {
-        if(!root) return nullptr;
-        if(root->val == start) return root;
-        TreeNode* left = findNode(root->left, start);
-        if (left) return left;
-        return findNode(root->right, start);
+    int result = INT_MIN;
+    int solve(TreeNode* root , int start){
+        if(root == NULL) return 0;
+        int LH = solve(root -> left , start);
+        int RH = solve(root -> right , start);
+        if(root -> val == start){
+            result = max(LH , RH);
+            return -1;
+        }
+        else if(LH >= 0 && RH >= 0){
+            return max(LH , RH)+1;
+        }
+        else{
+            int d = abs(LH) + abs(RH);
+            result = max(result , d);
+            return min(LH , RH)-1;
+        }
+        return 0;
     }
     int amountOfTime(TreeNode* root, int start) {
-        buildParent(root, nullptr);
-        TreeNode* startNode = findNode(root, start);
-        queue<TreeNode*> q;
-        unordered_set<TreeNode*> vis;
-        q.push(startNode);
-        vis.insert(startNode);
-        int time = -1;
-        while (!q.empty()) {
-            int sz = q.size();
-            time++;
-            while (sz--) {
-                TreeNode* curr = q.front();
-                q.pop();
-                if (curr->left && !vis.count(curr->left)) {
-                    vis.insert(curr->left);
-                    q.push(curr->left);
-                }
-                if (curr->right && !vis.count(curr->right)) {
-                    vis.insert(curr->right);
-                    q.push(curr->right);
-                }
-                if (parent[curr] && !vis.count(parent[curr])) {
-                    vis.insert(parent[curr]);
-                    q.push(parent[curr]);
-                }
-            }
-        }
-        return time;
+        solve(root , start);
+        return  result;   
     }
 };
