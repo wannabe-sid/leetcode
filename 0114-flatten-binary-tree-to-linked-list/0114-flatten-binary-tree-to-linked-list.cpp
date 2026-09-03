80f8f54450096e7ea02ad64df9a2278e58c5f6ee
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-09-04 00:13:25 +0530

    Time: 0 ms (100%), Space: 17.7 MB (24.18%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -12,27 +12,37 @@
 
 // Approach 1
 // O(n) time and O(n) stack space
-class Solution {
-public:
-    TreeNode* prev = nullptr;
-    void flatten(TreeNode* root) {
-        if(root == nullptr) return;
-        flatten(root -> right);
-        flatten(root -> left);
-        root -> right = prev;
-        root -> left = nullptr;
-        prev = root;
-    }
-};
-
-// Approach 2
-// O() time and O() space
 // class Solution {
 // public:
+//     TreeNode* prev = nullptr;
 //     void flatten(TreeNode* root) {
-        
+//         if(root == nullptr) return;
+//         flatten(root -> right);
+//         flatten(root -> left);
+//         root -> right = prev;
+//         root -> left = nullptr;
+//         prev = root;
 //     }
 // };
+
+// Approach 2
+// O(n) time and O(n) space
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(root == nullptr) return;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* curr = st.top();
+            st.pop();
+            if(curr -> right) st.push(curr -> right);
+            if(curr -> left) st.push(curr -> left);
+            if(!st.empty()) curr -> right = st.top();
+            curr -> left = nullptr;
+        }
+    }
+};
 
 // Approach 3
 // O() time and O() space
