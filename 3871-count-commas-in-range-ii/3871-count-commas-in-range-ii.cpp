d9dd40b7d731ccff9cfcabf112b56949e6b9107e
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-09-09 15:02:20 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     long long countCommas(long long n) {
         long long commas = 0;
-        if (n >= 1000LL) commas += (n - 1000LL + 1);
-        if (n >= 1000000LL) commas += (n - 1000000LL + 1);
-        if (n >= 1000000000LL) commas += (n - 1000000000LL + 1);
-        if (n >= 1000000000000LL) commas += (n - 1000000000000LL + 1);
-        if (n >= 1000000000000000LL) commas += (n - 1000000000000000LL + 1);
+        if (n >= 1000) commas += (n - 1000 + 1);
+        if (n >= 1000000) commas += (n - 1000000 + 1);
+        if (n >= 1000000000) commas += (n - 1000000000 + 1);
+        if (n >= 1000000000000) commas += (n - 1000000000000 + 1);
+        if (n >= 1000000000000000) commas += (n - 1000000000000000 + 1);
         return commas;
     }
 };
