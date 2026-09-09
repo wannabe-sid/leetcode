55c56d5f5f03ae2cb5e52f0f4296fc8fdd1e387a
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-09-09 14:45:55 +0530

    Time: 0 ms (100%), Space: 17.4 MB (79.68%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,21 +1,48 @@
 // Approach 1 : Using BFS
 // O(V + E) time and O(V) space
+// class Solution {
+// private: 
+//     bool colorComponentBFS(int start, vector<vector<int>>& graph, vector<int>& color){
+//         queue<int> q;
+//         q.push(start);
+//         color[start] = 0;
+//         while(!q.empty()){
+//             int curr = q.front();
+//             q.pop();
+//             for(auto it : graph[curr]){
+//                 if(color[it] == -1){
+//                     color[it] = !color[curr];
+//                     q.push(it);
+//                 }
+//                 else if(color[it] == color[curr]) return false;
+//             }
+//         }
+//         return true;
+//     }
+// public:
+//     bool isBipartite(vector<vector<int>>& graph) {
+//         int V = graph.size();
+//         vector<int> color(V, -1);
+//         for(int i=0; i<V; i++){
+//             if(color[i] == -1){
+//                 if(colorComponentBFS(i, graph, color) == false) return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+// Approach 2 : Using DFS
+// O(V + E) time and O(V) space
 class Solution {
-private: 
-    bool colorComponent(int start, vector<vector<int>>& graph, vector<int>& color){
-        queue<int> q;
-        q.push(start);
-        color[start] = 0;
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            for(auto it : graph[curr]){
-                if(color[it] == -1){
-                    color[it] = !color[curr];
-                    q.push(it);
-                }
-                else if(color[it] == color[curr]) return false;
+private:
+    bool colorComponentDFS(int start, int col, vector<vector<int>>& graph, vector<int>& color){
+        color[start] = col;
+        for(auto it : graph[start]){
+            if(color[it] == -1){
+                if(colorComponentDFS(it, !col, graph, color) == false) return false;
             }
+            else if(color[it] == col) return false;
         }
         return true;
     }
@@ -25,18 +52,9 @@ public:
         vector<int> color(V, -1);
         for(int i=0; i<V; i++){
             if(color[i] == -1){
-                if(colorComponent(i, graph, color) == false) return false;
+                if(colorComponentDFS(i, 0, graph, color) == false) return false;
             }
         }
         return true;
     }
 };
-
-// Approach 2 : Using DFS
-// O() time and O() space
-// class Solution {
-// public:
-//     bool isBipartite(vector<vector<int>>& graph) {
-        
-//     }
-// };
