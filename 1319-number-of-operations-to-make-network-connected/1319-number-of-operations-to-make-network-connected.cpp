35bbc5471a3095d9eae33440aca6b5bafa3ecf8b
Siddharth Singh <127819677+wannabe-sid@users.noreply.github.com>
2026-09-23 14:23:24 +0530

    Time: 75 ms (5.07%), Space: 42.6 MB (69.08%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,62 @@
+// O(V+E) time and O(V) space
+class DisjointSet{
+    vector<int> rank, size, parent;
+public:
+    DisjointSet(int n){
+        rank.resize(n+1, 0);
+        size.resize(n+1, 1);
+        parent.resize(n+1);
+        for(int i=0; i<=n; i++){
+            parent[i] = i;
+        }
+    }
+    int findParent(int node){
+        if(parent[node] == node) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    void unionByRank(int u, int v){
+        int ultParentU = findParent(u);
+        int ultParentV = findParent(v);
+        if(rank[ultParentU] < rank[ultParentV]){
+            parent[ultParentU] = ultParentV;
+        }
+        else if(rank[ultParentV] < rank[ultParentU]){
+            parent[ultParentV] = ultParentU;
+        }
+        else{
+            parent[ultParentV] = ultParentU;
+            rank[ultParentU]++;
+        }
+    }
+    void unionBySize(int u, int v){
+        int ultParentU = findParent(u);
+        int ultParentV = findParent(v);
+        if(size[ultParentU] < size[ultParentV]){
+            parent[ultParentU] = ultParentV;
+            size[ultParentV] += size[ultParentU];
+        }
+        else{
+            parent[ultParentV] = ultParentU;
+            size[ultParentU] += size[ultParentV];
+        }
+    }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        int ans = 0;
+        int extra = 0;
+        for(auto& edge : connections){
+            int u = edge[0];
+            int v = edge[1];
+            if(ds.findParent(u) == ds.findParent(v)) extra++;    
+            else ds.unionBySize(u, v);
+        }
+        for(int i=0; i<n; i++){
+            if(ds.findParent(i) == i) ans++;
+        }
+        if(extra >= ans - 1) return ans - 1;
+        return -1;
+    }
+};
